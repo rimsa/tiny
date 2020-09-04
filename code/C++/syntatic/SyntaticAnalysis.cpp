2d9e5f843f09ecfadc8dbb197138aa201ce84818
Andrei Rimsa <andrei@cefetmg.br>
2020-09-04 10:29:18 -0300

    [C++] Implementação do analisador sintático.

--- code/C++/syntatic/SyntaticAnalysis.cpp
+++ code/C++/syntatic/SyntaticAnalysis.cpp
@@ -57,57 +57,144 @@ void SyntaticAnalysis::procProgram() {
 
 // <cmdlist>   ::= <cmd> { <cmd> }
 void SyntaticAnalysis::procCmdList() {
-	// TODO: Implement me!
+	procCmd();
+	while (m_current.type == TT_VAR ||
+	       m_current.type == TT_OUTPUT ||
+		   m_current.type == TT_IF ||
+		   m_current.type == TT_WHILE) {
+		procCmd();
+	}
 }
 
 // <cmd>       ::= (<assign> | <output> | <if> | <while>) ;
 void SyntaticAnalysis::procCmd() {
-	// TODO: Implement me!
+	if (m_current.type == TT_VAR) {
+		procAssign();
+	} else if (m_current.type == TT_OUTPUT) {
+		procOutput();
+	} else if (m_current.type == TT_IF) {
+		procIf();
+	} else if (m_current.type == TT_WHILE) {
+		procWhile();
+	} else {
+		showError();
+	}
+
+	eat(TT_SEMICOLON);
 }
 
 // <assign>    ::= <var> = <intexpr>
 void SyntaticAnalysis::procAssign() {
-	// TODO: Implement me!
+	procVar();
+	eat(TT_ASSIGN);
+	procIntExpr();
 }
 
 // <output>    ::= output <intexpr>
 void SyntaticAnalysis::procOutput() {
-	// TODO: Implement me!
+	eat(TT_OUTPUT);
+	procIntExpr();
 }
 
 // <if>        ::= if <boolexpr> then <cmdlist> [ else <cmdlist> ] done
 void SyntaticAnalysis::procIf() {
-	// TODO: Implement me!
+	eat(TT_IF);
+	procBoolExpr();
+	eat(TT_THEN);
+	procCmdList();
+	if (m_current.type == TT_ELSE) {
+		advance();
+		procCmdList();
+	}
+	eat(TT_DONE);
 }
 
 // <while>     ::= while <boolexpr> do <cmdlist> done
 void SyntaticAnalysis::procWhile() {
-	// TODO: Implement me!
+	eat(TT_WHILE);
+	procBoolExpr();
+	eat(TT_DO);
+	procCmdList();
+	eat(TT_DONE);
 }
 
 // <boolexpr>  ::= false | true |
 //                 not <boolexpr> |
 //                 <intterm> (== | != | < | > | <= | >=) <intterm>
 void SyntaticAnalysis::procBoolExpr() {
-	// TODO: Implement me!
+	if (m_current.type == TT_FALSE) {
+		advance();
+	} else if (m_current.type == TT_TRUE) {
+		advance();
+	} else if (m_current.type == TT_NOT) {
+		advance();
+		procBoolExpr();
+	} else {
+		procIntTerm();
+
+		switch (m_current.type) {
+			case TT_EQUAL:
+			case TT_NOT_EQUAL:
+			case TT_LOWER:
+			case TT_GREATER:
+			case TT_LOWER_EQUAL:
+			case TT_GREATER_EQUAL:
+				advance();
+				break;
+			default:
+				showError();
+		}
+
+		procIntTerm();
+	}
 }
 
 // <intexpr>   ::= [ + | - ] <intterm> [ (+ | - | * | / | %) <intterm> ]
 void SyntaticAnalysis::procIntExpr() {
-	// TODO: Implement me!
+	if (m_current.type == TT_ADD) {
+		advance();
+	} else if (m_current.type == TT_SUB) {
+		advance();
+	}
+
+	procIntTerm();
+
+	if (m_current.type == TT_ADD ||
+	    m_current.type == TT_SUB ||
+		m_current.type == TT_MUL ||
+	    m_current.type == TT_DIV ||
+	    m_current.type == TT_MOD) {
+		switch (m_current.type) {
+			case TT_ADD:
+			case TT_SUB:
+			case TT_MUL:
+			case TT_DIV:
+			case TT_MOD:
+			default:
+				advance();
+		}
+
+		procIntTerm();
+	}
 }
 
 // <intterm>   ::= <var> | <const> | read
 void SyntaticAnalysis::procIntTerm() {
-	// TODO: Implement me!
+	if (m_current.type == TT_VAR) {
+		procVar();
+	} else if (m_current.type == TT_NUMBER) {
+		procConst();
+	} else {
+		eat(TT_READ);
+	}
 }
 
 // <var>       ::= id
 void SyntaticAnalysis::procVar() {
-	// TODO: Implement me!
+	eat(TT_VAR);
 }
 
 // <const>     ::= number
 void SyntaticAnalysis::procConst() {
-	// TODO: Implement me!
+	eat(TT_NUMBER);
 }
